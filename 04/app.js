7743a9a1dc02f262309bed50b1d6b8c136c2b732
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-21 14:31:16 +0200

    04 done

--- 04/app.js
+++ 04/app.js
@@ -1,11 +1,26 @@
 const divsList = document.querySelectorAll('div')
 const bodyElement = document.querySelector('body')
 
-const handleClick = function(e){
-    e.currentTarget.classList.add('clicked')
+const handleClick = function (e) {
+    const thisDiv = this
 
+    setTimeout(function () {
+        thisDiv.classList.add('clicked')
+
+    }, thisDiv.dataset.time)
+
+    if (this.tagName === 'BODY') {
+        e.stopPropagation()
+    }
 }
 
-divsList.forEach(function(div){
+divsList.forEach(function (div) {
     div.addEventListener('click', handleClick)
+})
+
+bodyElement.addEventListener('click', function () {
+    divsList.forEach(function (div) {
+        div.classList.remove('clicked')
+
+    })
 })
